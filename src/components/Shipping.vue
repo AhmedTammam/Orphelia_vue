3cb93fbe6f6663c9b2e73abcf4a0f48b424f39ac
Ahmed Tammam <tamam65@gmail.com>
2018-08-11 17:23:13 +0200

    styling

--- src/components/Shipping.vue
+++ src/components/Shipping.vue
@@ -1,25 +1,27 @@
 <template>
     <section class="shipping">
-        <div class="row">
-            <div class="col-md-4 shipp-item">
-                <img src="@/assets/img/gem.svg" alt="GUARANTEE">
-                <div>
-                    <h5 class="mt-0">2 Years</h5>
-                    <h5>GUARANTEE</h5>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-xs-12 shipp-item">
+                    <img src="@/assets/img/gem.svg" alt="GUARANTEE">
+                    <div>
+                        <h5 class="mt-0">2 Years</h5>
+                        <h5>GUARANTEE</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-4 shipp-item">
-                <img src="@/assets/img/shipp.svg" alt="Shipping">
-                <div>
-                    <h5 class="mt-0">Free</h5>
-                    <h5>SHIPPING</h5>
+                <div class="col-md-4 col-sm-12 col-xs-12 shipp-item">
+                    <img src="@/assets/img/shipp.svg" alt="Shipping">
+                    <div>
+                        <h5 class="mt-0">Free</h5>
+                        <h5>SHIPPING</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-4 shipp-item">
-                <img src="@/assets/img/returns.svg" alt="Returns">
-                <div>
-                    <h5 class="mt-0">Free</h5>
-                    <h5>RETURNS</h5>
+                <div class="col-md-4 col-sm-12 shipp-item">
+                    <img src="@/assets/img/returns.svg" alt="Returns">
+                    <div>
+                        <h5 class="mt-0">Free</h5>
+                        <h5>RETURNS</h5>
+                    </div>
                 </div>
             </div>
         </div>
