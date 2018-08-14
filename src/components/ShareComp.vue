95688dc3fb5473e77bf548756c6499c4286a4a7a
Ahmed Tammam <tamam65@gmail.com>
2018-08-14 10:35:25 +0200

    shere it V.1

--- src/components/ShareComp.vue
+++ src/components/ShareComp.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="share">
         <div class="container">
-            <div class="row text-left">
-                <div>
+            <div class="row">
+                <div class="fix">
                     <h3>SHARE IT TO WEAR IT</h3>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-8 col-md-6">
-                    <p class="ml-4">With every share you get another entry into the draw </p>
+                    <p>With every share you get another entry into the draw </p>
                 </div>
                 <div class="col-lg-4 col-md-6 text-center">
                     <a href="http://"><img src="@/assets/img/fb.svg" alt="facebook"></a>
